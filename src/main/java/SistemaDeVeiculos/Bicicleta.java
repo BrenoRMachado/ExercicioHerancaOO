6639480ae4154f959bbfe3739b345a72c203a04b
Breno Machado <brenomachado.3064@gmail.com>
2025-05-29 15:37:51 -0300

    Realiza as implementações das subclasses

--- src/main/java/SistemaDeVeiculos/Bicicleta.java
+++ src/main/java/SistemaDeVeiculos/Bicicleta.java
@@ -1,4 +1,13 @@
 package SistemaDeVeiculos;
 
-public class Bicicleta extends Veiculo{
+public class Bicicleta extends Veiculo {
+
+    public Bicicleta(String marca, String modelo, double precoBase) {
+        super(marca, modelo, precoBase);
+    }
+
+    @Override
+    public double calcularCusto() {
+        return getPrecoBase() * 1.05;
+    }
 }
--- src/main/java/SistemaDeVeiculos/Moto.java
+++ src/main/java/SistemaDeVeiculos/Moto.java
@@ -1,4 +1,13 @@
 package SistemaDeVeiculos;
 
-public class Moto extends Veiculo{
+public class Moto extends Veiculo {
+
+    public Moto(String marca, String modelo, double precoBase) {
+        super(marca, modelo, precoBase);
+    }
+
+    @Override
+    public double calcularCusto() {
+        return getPrecoBase() * 1.1;
+    }
 }
