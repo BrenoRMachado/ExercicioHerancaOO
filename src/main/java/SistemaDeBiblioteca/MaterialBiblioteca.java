8c506a41c33b55d91fa629259ed58d116537bbe3
Breno Machado <brenomachado.3064@gmail.com>
2025-05-29 15:14:45 -0300

    Implementa MaterialBiblioteca

--- src/main/java/SistemaDeBiblioteca/MaterialBiblioteca.java
+++ src/main/java/SistemaDeBiblioteca/MaterialBiblioteca.java
@@ -1,5 +1,36 @@
 package SistemaDeBiblioteca;
 
 public abstract class MaterialBiblioteca {
-    
+
+    private String titulo;
+    private String dataPublicacao; // Agora como String
+
+    public MaterialBiblioteca(String titulo, String dataPublicacao) {
+        setTitulo(titulo);
+        setDataPublicacao(dataPublicacao);
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        if (titulo.trim().isEmpty()) {
+            throw new IllegalArgumentException("Título não pode ser vazio");
+        }
+        this.titulo = titulo;
+    }
+
+    public String getDataPublicacao() {
+        return this.dataPublicacao;
+    }
+
+    public void setDataPublicacao(String dataPublicacao) {
+        if (dataPublicacao.trim().isEmpty()) {
+            throw new IllegalArgumentException("Data de publicação não pode ser vazia");
+        }
+        this.dataPublicacao = dataPublicacao;
+    }
+
+    public abstract String calcularDataDevolucao(String dataEmprestimo);
 }
